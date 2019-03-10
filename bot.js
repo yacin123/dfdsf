2c3174d3968ae5970fc4d4957d8a0560a2da0d49
yacin123 <44523421+yacin123@users.noreply.github.com>
2019-03-10 21:15:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("الملوك",{type: 'WATCHING'})
+   client.user.setActivity("Yasin.FM",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
